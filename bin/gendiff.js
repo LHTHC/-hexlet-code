a4c7b3e940df29fc830cb15ccc3e2d25c6f02a3a
Nikita L <starfall1011@gmail.com>
2021-08-16 13:47:44 +0300

    add comparing two flat json's

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -2,12 +2,16 @@
 
 import { Command } from 'commander';
 const program = new Command();
+import calcDiff from '../src/index.js';
 
 program
   .name('gendiff')
   .usage('[options] <filepath1> <filepath2>')
   .description('Compares two configuration files and shows a difference.')
   .version('0.0.1', 'V, --vers', 'output the current version')
+  .arguments('<filepath1> <filepath2>')
   .option('-f, --format [type]', 'output format')
-  
+  .action((file1, file2) => {
+    console.log(calcDiff(file1, file2));
+  })
   program.parse(process.argv);
---
+++ src/index.js
@@ -0,0 +1,30 @@
+import fs from 'fs';
+import path from 'path';
+import _ from 'lodash';
+
+export const readFile = (file) => fs.readFileSync(path.resolve(process.cwd(), './__tests__/fixtures', file), 'utf-8');
+
+export const calcDiff = (file1, file2) => {
+    const data1 = JSON.parse(readFile(file1));
+    const data2 = JSON.parse(readFile(file2));
+
+    const keys = _.orderBy(_.union(Object.keys(data1), Object.keys(data2)));
+
+    const result = keys.map((key) => {
+        if (_.has(data1, key) && _.has(data2, key) && data1[key] === data2[key]) {
+            return `    ${key}: ${data1[key]}\n`;
+        }
+        if (_.has(data1, key) && !_.has(data2, key)) {
+            return `  - ${key}: ${data1[key]}\n`;
+        }
+        if (!_.has(data1, key) && _.has(data2, key)) {
+            return `  + ${key}: ${data1[key]}\n`;
+        }
+        if (_.has(data1, key) && _.has(data2, key) && data1[key] !== data2[key]) {
+            return `  - ${key}: ${data1[key]}\n  + ${[key]}: ${data2[key]}\n`;
+        }
+    })
+    return `{\n${result.join('')}}`;
+};
+
+export default calcDiff;
