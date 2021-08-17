da72877247575e01103819c9f16e1e8434aff6e6
Nikita L <starfall1011@gmail.com>
2021-08-17 13:05:45 +0300

    linter fixes

--- __tests__/calcDiff.test.js
+++ __tests__/calcDiff.test.js
@@ -1,10 +1,10 @@
 import { readFileSync } from 'fs';
 import path from 'path';
 import { expect } from '@jest/globals';
-import calcDiff from '../src/calcDiff';
 import { fileURLToPath } from 'url';
+import calcDiff from '../src/calcDiff';
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url))
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const firstAnswerPath = path.resolve(__dirname, '__fixtures__/plain.txt');
 const firstAnswer = readFileSync(firstAnswerPath, 'utf-8');
 test('calcDiff', () => {
