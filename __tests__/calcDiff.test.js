a2d29b4cc712a788f27e208371441ff758c81f6a
Nikita L <starfall1011@gmail.com>
2021-08-18 16:55:22 +0300

    update plain tests

--- __tests__/calcDiff.test.js
+++ __tests__/calcDiff.test.js
@@ -8,11 +8,20 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const firstAnswerPath = path.resolve(__dirname, '__fixtures__/nested.txt');
 const firstAnswer = readFileSync(firstAnswerPath, 'utf-8');
-
+const secondAnswerPath = path.resolve(__dirname, '__fixtures__/plain.txt');
+const secondAnswer = readFileSync(secondAnswerPath, 'utf-8');
 test('calcDiff Nested JSON', () => {
-  expect(calcDiff('file1.json', 'file2.json')).toEqual(firstAnswer);
+  expect(calcDiff('file1.json', 'file2.json', 'stylish')).toEqual(firstAnswer);
 });
 
 test('calcDiff Nested YAML', () => {
-  expect(calcDiff('file1.yaml', 'file2.yml')).toEqual(firstAnswer);
+  expect(calcDiff('file1.yaml', 'file2.yml', 'stylish')).toEqual(firstAnswer);
+});
+
+test('calcDiff Plain YAML', () => {
+  expect(calcDiff('file1.yaml', 'file2.yml', 'plain')).toEqual(secondAnswer);
+});
+
+test('calcDiff Plain JSON', () => {
+  expect(calcDiff('file1.json', 'file2.json', 'plain')).toEqual(secondAnswer);
 });
