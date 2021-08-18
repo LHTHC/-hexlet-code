1964db3954075c36ebdf9245471f450aa4a72acb
Nikita L <starfall1011@gmail.com>
2021-08-18 16:07:18 +0300

    set default format

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -10,8 +10,8 @@ program
   .description('Compares two configuration files and shows a difference.')
   .version('0.0.1', 'V, --vers', 'output the current version')
   .arguments('<filepath1> <filepath2>')
-  .option('-f, --format [type]', 'output format')
+  .option('-f, --format [type]', 'output format', 'stylish', 'stylish')
   .action((file1, file2) => {
-    console.log(genDiff(file1, file2));
-  });
-program.parse(process.argv);
+    console.log(genDiff(file1, file2, program.opts().format));
+  })
+  .parse(process.argv);
