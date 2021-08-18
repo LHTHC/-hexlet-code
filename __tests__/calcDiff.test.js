e972693801c49d7db54fc41fc933eeba2569d73e
Nikita L <starfall1011@gmail.com>
2021-08-18 12:24:02 +0300

    add yaml plain tests

--- __tests__/calcDiff.test.js
+++ __tests__/calcDiff.test.js
@@ -2,11 +2,15 @@ import { readFileSync } from 'fs';
 import path from 'path';
 import { expect } from '@jest/globals';
 import { fileURLToPath } from 'url';
-import calcDiff from '../src/calcDiff';
+import calcDiff from '../src/index';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const firstAnswerPath = path.resolve(__dirname, '__fixtures__/plain.txt');
 const firstAnswer = readFileSync(firstAnswerPath, 'utf-8');
-test('calcDiff', () => {
+test('calcDiffPlainJSON', () => {
   expect(calcDiff('file1.json', 'file2.json')).toEqual(firstAnswer);
+});
+
+test('calcDiffPlainYAML', () => {
+  expect(calcDiff('file1.yaml', 'file2.yml')).toEqual(firstAnswer);
 });
