41be2024fde7c1b977ed5def811880b532667c85
Nikita L <starfall1011@gmail.com>
2021-08-18 13:21:28 +0300

    add test for nested JSON's and YAML's

--- __tests__/calcDiff.test.js
+++ __tests__/calcDiff.test.js
@@ -5,12 +5,14 @@ import { fileURLToPath } from 'url';
 import calcDiff from '../src/index';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
-const firstAnswerPath = path.resolve(__dirname, '__fixtures__/plain.txt');
+
+const firstAnswerPath = path.resolve(__dirname, '__fixtures__/nested.txt');
 const firstAnswer = readFileSync(firstAnswerPath, 'utf-8');
-test('calcDiffPlainJSON', () => {
+
+test('calcDiff Nested JSON', () => {
   expect(calcDiff('file1.json', 'file2.json')).toEqual(firstAnswer);
 });
 
-test('calcDiffPlainYAML', () => {
+test('calcDiff Nested YAML', () => {
   expect(calcDiff('file1.yaml', 'file2.yml')).toEqual(firstAnswer);
 });
